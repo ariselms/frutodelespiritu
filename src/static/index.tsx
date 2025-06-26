a238448b4a435fd79e6e9cc28e8c7545736940fc
Reynaldo Navedo <rnavedo@ariselms.com>
2025-06-26 15:20:56 -0400

    minor issues

--- src/static/index.tsx
+++ src/static/index.tsx
@@ -45,11 +45,11 @@ if (process.env.NEXT_PUBLIC_VERCEL_ENV === "development") {
 }
 
 if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
-	baseUrl = "https://familiaprint-dev.vercel.app";
+	baseUrl = "https://frutodelespiritu-dev.vercel.app";
 }
 
 if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
-	baseUrl = "https://www.familiaprint.com";
+	baseUrl = "https://frutodelespiritu.vercel.app/";
 }
 
 export const serverBaseUrl = baseUrl;
