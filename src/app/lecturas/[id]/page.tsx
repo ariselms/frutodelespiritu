33b78a87a7a52db9878854b38b5da3d997773dac
Reynaldo Navedo <rnavedo@ariselms.com>
2025-06-28 02:13:56 -0400

    fixed type issue with params passed to server side page

--- src/app/lecturas/[id]/page.tsx
+++ src/app/lecturas/[id]/page.tsx
@@ -4,12 +4,16 @@ import Link from "next/link";
 import "../../articles.css";
 import Image from "next/image";
 
+interface PageProps {
+  params: { id: string };
+}
+
 export default async function SingleLecturePage({
 	params
 }: {
-	params: { id: string };
+	params: Promise<{ id: string }>;
 }) {
-	const { id } = await params;
+  const { id } = await params;
 
 	let FetchedDetailedArticled;
   let FetchedRecentArticles;
