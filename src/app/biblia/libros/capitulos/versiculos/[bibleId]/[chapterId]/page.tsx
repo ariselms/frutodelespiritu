63a1e03d83ed330558e6818299ae4c7eacda1790
Reynaldo Navedo <rnavedo@ariselms.com>
2025-07-02 09:55:56 -0400

    progress on bible book chapters

--- src/app/biblia/libros/capitulos/versiculos/[bibleId]/[chapterId]/page.tsx
+++ src/app/biblia/libros/capitulos/versiculos/[bibleId]/[chapterId]/page.tsx
@@ -1,5 +1,6 @@
 import BibleHeaderSection from "@/components/layout/BibleSection";
 import Link from "next/link";
+import "./bible-chapter.css";
 
 export default async function SingleChapterPage({
 	params
@@ -31,11 +32,11 @@ export default async function SingleChapterPage({
 
     BibleChapter = await chaptersRequest.json();
 
-    console.log(BibleChapter.data);
-
   } catch (error) {
     console.error(error);
   }
+
+  // TODO: Add notes functionality as a top bar as well as commentary from the API in a new sort of modal
 
 	return (
 		<main>
@@ -48,7 +49,23 @@ export default async function SingleChapterPage({
 						{BibleChapter.data.previous && (
 							<Link
 								href={`/biblia/libros/capitulos/versiculos/${BibleChapter.data.bibleId}/${BibleChapter.data.previous.id}`}
-								className="rounded-xl border border-orange-400 dark:border-gray-600 bg-orange-100 dark:bg-gray-800 px-5 py-1 font-bold text-orange-700 dark:text-gray-50">
+								className="rounded-xl border border-orange-400 dark:border-gray-600 bg-orange-100 dark:bg-gray-800 px-5 py-1 font-bold text-orange-700 dark:text-gray-50 flex items-center">
+								<svg
+									className="w-6 h-6 text-orange-700 dark:text-white"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									fill="none"
+									viewBox="0 0 24 24">
+									<path
+										stroke="currentColor"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="m15 19-7-7 7-7"
+									/>
+								</svg>
 								{BibleChapter.data.previous.bookId}{" "}
 								{BibleChapter.data.previous.number}
 							</Link>
@@ -57,13 +74,29 @@ export default async function SingleChapterPage({
 						{BibleChapter.data.next && (
 							<Link
 								href={`/biblia/libros/capitulos/versiculos/${BibleChapter.data.bibleId}/${BibleChapter.data.next.id}`}
-								className="rounded-xl border border-orange-400 dark:border-gray-600 bg-orange-100 dark:bg-gray-800 px-5 py-1 font-bold text-orange-700 dark:text-gray-50">
+								className="rounded-xl border border-orange-400 dark:border-gray-600 bg-orange-100 dark:bg-gray-800 px-5 py-1 font-bold text-orange-700 dark:text-gray-50 flex items-center">
 								{BibleChapter.data.next.bookId} {BibleChapter.data.next.number}
+								<svg
+									className="w-6 h-6 text-orange-700 dark:text-white"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									fill="none"
+									viewBox="0 0 24 24">
+									<path
+										stroke="currentColor"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="m9 5 7 7-7 7"
+									/>
+								</svg>
 							</Link>
 						)}
 					</nav>
 					<div
-						className="max-w-[80ch] mx-auto"
+						className="bible-chapter max-w-[80ch] mx-auto"
 						dangerouslySetInnerHTML={{ __html: BibleChapter.data.content }}
 					/>
 				</div>
