8b018f4cd64cb4f0993cffa81669470d7fe2c607
Reynaldo Navedo <rnavedo@ariselms.com>
2025-07-02 15:09:25 -0400

    minor improvements

--- src/components/layout/BibleSection.tsx
+++ src/components/layout/BibleSection.tsx
@@ -7,9 +7,9 @@ export default function BibleHeaderSection({
 }) {
 	return (
     <div className="mb-8 py-2">
-			<div className="max-w-screen-xl px-4 py-0 mx-auto flex items-center">
-        <Image width={60} height={60} src="/images/bible.svg" alt="mockup" className="mr-4" />
-        <h1 className="max-w-2xl text-4xl font-extrabold tracking-tight leading-none  text-orange-700 dark:text-white">
+			<div className="max-w-screen-xl px-4 py-0 mx-auto flex items-center justify-center">
+        <Image width={60} height={60} src="/images/bible.svg" alt="mockup" className="mr-2" />
+        <h1 className="ml-2 max-w-2xl text-4xl font-extrabold tracking-tight leading-none  text-orange-700 dark:text-white">
           {section}
         </h1>
 			</div>
