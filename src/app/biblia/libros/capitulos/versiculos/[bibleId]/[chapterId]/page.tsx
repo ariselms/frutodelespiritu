bf2990bb2f3b6f27e1d0b9f999a82c864a22b6c1
Reynaldo Navedo <rnavedo@ariselms.com>
2025-07-02 13:44:30 -0400

    updates to design in bible workflow removing duplicated indicator

--- src/app/biblia/libros/capitulos/versiculos/[bibleId]/[chapterId]/page.tsx
+++ src/app/biblia/libros/capitulos/versiculos/[bibleId]/[chapterId]/page.tsx
@@ -94,7 +94,6 @@ export default async function SingleChapterPage({
 							/>
 						</svg>
 						Capítulos de {BibleChapter.data.bookId}
-						Capítulos de {BibleChapter.data.bookId}
 					</Link>
 					<BibleHeaderSection
 						section={`Leyendo ${BibleChapter.data.reference}`}
