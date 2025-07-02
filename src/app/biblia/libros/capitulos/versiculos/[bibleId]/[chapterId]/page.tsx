3c14ee4105704cef1275308dc8ad89a44a2f5713
Reynaldo Navedo <rnavedo@ariselms.com>
2025-07-02 13:09:57 -0400

    minor updates to bible workglow

--- src/app/biblia/libros/capitulos/versiculos/[bibleId]/[chapterId]/page.tsx
+++ src/app/biblia/libros/capitulos/versiculos/[bibleId]/[chapterId]/page.tsx
@@ -57,6 +57,8 @@ export default async function SingleChapterPage({
 
 		// `chaptersRequestResult` already holds the resolved JSON data
 		BibleChapter = chaptersRequestResult;
+
+    console.log(BibleChapter.data);
 	} catch (error) {
 		// This catch block will now catch errors thrown by the `reject` call inside the Promise,
 		// or any unexpected errors during the Promise's execution.
@@ -90,7 +92,7 @@ export default async function SingleChapterPage({
 								d="m15 19-7-7 7-7"
 							/>
 						</svg>
-						Capítulos
+						Capítulos de {BibleChapter.data.bookId}
 					</Link>
 					<BibleHeaderSection
 						section={`Leyendo ${BibleChapter.data.reference}`}
