784c3eccf4d676710251f8b68331a9d8f27ade9c
Reynaldo Navedo <rnavedo@ariselms.com>
2025-07-02 15:06:09 -0400

    minor improvements

--- src/components/bible/BibleNavigationAndNotes.tsx
+++ src/components/bible/BibleNavigationAndNotes.tsx
@@ -39,9 +39,9 @@ export function BibleNavigationAndNotes({BibleChapterData}: any) {
 						fill="currentColor"
 						viewBox="0 0 24 24">
 						<path
-							fill-rule="evenodd"
+							fillRule="evenodd"
 							d="M10.271 5.575C8.967 4.501 7 5.43 7 7.12v9.762c0 1.69 1.967 2.618 3.271 1.544l5.927-4.881a2 2 0 0 0 0-3.088l-5.927-4.88Z"
-							clip-rule="evenodd"
+							clipRule="evenodd"
 						/>
 					</svg>
 				</Link>
