e693aad38a9e5ea8e532479e26e0e2b1228c490c
Reynaldo Navedo <rnavedo@ariselms.com>
2025-08-13 20:01:22 -0400

    added saved lectures functionality from user interface

---
+++ src/app/api/user/lectures/saved/route.ts
@@ -0,0 +1,74 @@
+import { NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
+export async function GET(request: Request) {
+	try {
+		const { searchParams } = new URL(request.url);
+
+		const userId = Number(searchParams.get("userId"));
+
+    const { rows: userLectures } = await sql`
+      SELECT
+        lectures.*
+      FROM lectures
+      JOIN saved_lectures ON saved_lectures.saved_lecture_id = lectures.id
+      WHERE
+        saved_lectures.saved_by_user_id = ${Number(userId)}
+      ORDER BY lectures.created_at DESC
+    `;
+
+		if (userLectures.length > 0) {
+			return NextResponse.json(
+				{
+					success: true,
+					message: "Lecturas solicitadas correctamente.",
+					data: userLectures
+				},
+				{ status: 200 }
+			);
+		}
+
+	} catch (error) {
+
+    console.error("Error fetching user lectures:", error);
+
+		return NextResponse.json({
+      success: false,
+      message: "Error obteniendo lectura.",
+      data: null
+    });
+
+
+	}
+}
+
+// DELETE handler for removing a saved lecture
+export async function DELETE(request: Request) {
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const userId = Number(searchParams.get("userId"));
+    const lectureId = Number(searchParams.get("lectureId"));
+
+    await sql`
+      DELETE FROM saved_lectures
+      WHERE saved_by_user_id = ${Number(userId)}
+      AND saved_lecture_id = ${Number(lectureId)}
+    `;
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Lectura guardada removida correctamente.",
+        data: null
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error removing saved lecture:", error);
+    return NextResponse.json({
+      success: false,
+      message: "Error eliminando lectura guardada.",
+      data: null
+    });
+  }
+}
--- src/app/perfil/lecturas/page.tsx
+++ src/app/perfil/lecturas/page.tsx
@@ -1,12 +1,177 @@
-import ProfileSection from "@/components/admin/ProfileSection";
+"use client";
 
-export default function UserLecturesPage(){
+import { useEffect, useState } from "react";
+import { useAuthContext } from "@/context/authContext";
+import {
+	Button,
+	List,
+	ListItem,
+	Modal,
+	ModalBody,
+	ModalHeader
+} from "flowbite-react";
+import Link from "next/link";
+import { ArticleType } from "@/models/articlesTypes";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { toast } from "react-toastify";
+
+export default function UserLecturesPage() {
+  const { user } = useAuthContext();
+
+  const [userLectures, setUserLectures] = useState<ArticleType[]>([]);
+
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  const [idForDelete, setIdForDelete] = useState<number | null>(null);
+
+
+  const fetchUserLectures = async () => {
+
+    setLoading(true);
+
+    try {
+
+      const request = await fetch(`/api/user/lectures/saved?userId=${user.id}`);
+
+      if (!request.ok) {
+        throw new Error("Failed to fetch user lectures");
+      }
+
+      const response = await request.json();
+
+      setUserLectures(response.data);
+
+    } catch (error) {
+
+      console.error("Error fetching user lectures:", error);
+
+    } finally {
+
+      setLoading(false);
+
+    }
+  }
+
+  useEffect(() => {
+
+    if (!user) {
+      return;
+    }
+
+    fetchUserLectures();
+
+  }, [user]);
+
+  const handleSavedLectureRemoved = async (lectureId: string) => {
+
+    console.log("Removing lecture with ID:", lectureId);
+
+    try {
+
+      const request = await fetch(`/api/user/lectures/saved?userId=${user.id}&lectureId=${lectureId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      });
+
+      if (!request.ok) {
+        throw new Error("Failed to remove saved lecture");
+      }
+
+      const response = await request.json();
+
+      if (response.success) {
+
+        toast.success(response.message);
+
+        fetchUserLectures();
+
+      } else {
+
+        toast.error(response.message);
+
+      }
+
+
+    } catch (error) {
+
+      console.error("Error removing saved lecture:", error);
+
+    }
+  }
+
   return (
 		<main className="dark:bg-gray-800">
 			<article className="container mx-auto px-2 text-black dark:text-white py-16">
-				Mis lecturas guardads y mis recortes de lecturas con notas aparecerán
-				aqui.
+				<p className="mb-8 text-lg max-w-[80ch]">
+					Mis lecturas guardads y mis recortes de lecturas con notas aparecerán
+					aqui. Por el momento los recortes de lecturas no están implementados.
+					Espera una actualización pronto.
+				</p>
+
+				{loading && (
+					<div className="text-center text-black dark:text-white">
+						Cargando...
+					</div>
+				)}
+
+				{userLectures.length > 0 ? (
+					<List className="max-w-5xl divide-y divide-orange-200 dark:divide-gray-700 w-full">
+						{userLectures.map((lecture: ArticleType) => (
+							<ListItem
+								className="w-full flex items-center justify-between list-none py-2"
+								key={lecture.id}>
+								<Link
+									className="text-orange-600 dark:text-white underline"
+									href={`/lecturas/${lecture.slug}`}>
+									{lecture.title}
+								</Link>
+								<Button
+									onClick={() => {
+										setOpenModal(true);
+										setIdForDelete(lecture.id);
+									}}
+									color="red"
+									className="cursor-pointer rounded-2xl">
+									Remover
+								</Button>
+							</ListItem>
+						))}
+					</List>
+				) : null}
 			</article>
+
+			<Modal
+				show={openModal}
+				size="md"
+				onClose={() => setOpenModal(false)}
+				popup>
+				<ModalHeader />
+				<ModalBody>
+					<div className="text-center">
+						<HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+						<h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+							Seguro que deseas remover esta lectura?
+						</h3>
+						<div className="flex justify-center gap-4">
+							<Button
+								color="red"
+								onClick={() => {
+									setOpenModal(false);
+									handleSavedLectureRemoved(String(idForDelete));
+								}}>
+								Si, estoy segur@
+							</Button>
+							<Button color="alternative" onClick={() => setOpenModal(false)}>
+								No, cancela
+							</Button>
+						</div>
+					</div>
+				</ModalBody>
+			</Modal>
 		</main>
 	);
 }
