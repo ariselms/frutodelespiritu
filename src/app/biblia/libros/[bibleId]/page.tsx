2fc6cd7e045447d8c9b3efa367a96c6583ac0c12
Reynaldo <rnavedo@ariselms.com>
2025-08-23 16:53:19 -0400

    minor updates

--- src/app/biblia/libros/[bibleId]/page.tsx
+++ src/app/biblia/libros/[bibleId]/page.tsx
@@ -71,20 +71,33 @@ export default async function BibleBooksPage({
 	const bibleInfo = await BibleInfo.json();
 	const books = await BibleBooks.json();
 
-	const pentateuco = books?.data.slice(0, 5);
-	const historicos = books?.data.slice(5, 17);
-	const poetiocs = books?.data.slice(17, 22);
-	const profetas = books?.data.slice(22, 39);
-	const evangelios = books?.data.slice(39, 43);
-	const historico = books?.data.slice(43, 44);
-	const cartas = books?.data.slice(44, 65);
-	const revelaciones = books?.data.slice(65, 66);
+	if(books.message === "bad api-key") return <div className="text-center p-4 max-w-[80ch] mx-auto">Lo sentimos, hubo un error, intente nuevamente...</div>;
+
+	let pentateuco;
+	let historicos;
+	let poetiocs;
+	let profetas;
+	let evangelios;
+	let historico;
+	let cartas;
+	let revelaciones;
+
+	if(books.size > 0) {
+		pentateuco	= books?.data.slice(0, 5);
+		historicos	= books?.data.slice(5, 17);
+		poetiocs	= books?.data.slice(17, 22);
+		profetas	= books?.data.slice(22, 39);
+		evangelios	= books?.data.slice(39, 43);
+		historico	= books?.data.slice(43, 44);
+		cartas	= books?.data.slice(44, 65);
+		revelaciones	= books?.data.slice(65, 66);
+	}
 
 	return (
 		<main>
 			<section className="w-full dark:bg-gray-800 text-gray-800">
 				<div className="max-w-7xl mx-auto py-8 px-2 xl:px-0">
-					<BibleHeaderSection section={bibleInfo.data.nameLocal} />
+					<BibleHeaderSection section={bibleInfo?.data?.nameLocal} />
 
 					<h1 className="text-2xl lg:text-3xl font-bold text-center mt-4 mb-8 text-gray-600 dark:text-gray-300">
 						Antiguo Testamento
