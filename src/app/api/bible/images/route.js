101e6295833a9b26cb55e10baad0260018b256e0
Reynaldo Navedo <rnavedo@ariselms.com>
2025-12-05 00:46:57 -0500

    minor updates

--- src/app/api/bible/images/route.js
+++ src/app/api/bible/images/route.js
@@ -1,110 +1,145 @@
 import { ImageResponse } from "next/og";
-
-export const runtime = "edge";
+import { ApiExportBaseImage } from "@/components/media";
 
 export async function GET(request) {
 	try {
+    // get the query params
+    const { searchParams } = new URL(request.url);
+
+    const translation = searchParams.get('translation');
+    const book = searchParams.get('book');
+    const chapter = searchParams.get('chapter');
+    const verseFrom = searchParams.get('verseFrom');
+    const verseTo = searchParams.get('verseTo');
+    const passageText = searchParams.get('passageText');
+    const selectedBackgroundImageValue = searchParams.get(
+			"selectedBackgroundImageValue"
+		);
+    const verseInfoSize = searchParams.get("verseInfoSize");
+    const passageTextSize = searchParams.get("passageTextSize");
+    const usage = searchParams.get("usage");
+
+    const passageSelection = {
+      translation: translation,
+      book: {name: book},
+      chapter: chapter,
+      verseFrom: verseFrom,
+      verseTo: verseTo,
+      passageText: passageText,
+    }
+
+    const mediaControllers = {
+			selectedBackgroundImageValue: selectedBackgroundImageValue,
+			verseInfoSize: verseInfoSize,
+			passageTextSize: passageTextSize
+		};
+
 		// 3. Return the ImageResponse
 		return new ImageResponse(
 			(
-				<div
-					style={{
-						height: "100%",
-						width: "100%",
-						display: "flex",
-						textAlign: "center",
-						alignItems: "flex-start",
-						justifyContent: "space-between",
-						flexDirection: "column",
-						flexWrap: "nowrap",
-						backgroundColor: "white",
-						padding: 16,
-						backgroundImage:
-							"linear-gradient(to bottom left, #FED7AA, #FFEDD5, #FFF7ED)",
-						backgroundRepeat: "no-repeat"
-					}}>
-					<div
-						style={{
-							position: "absolute",
-							right: 16,
-							bottom: 16,
-							display: "flex",
-							alignItems: "stretch",
-							flexDirection: "row-reverse"
-						}}>
-						<img
-							src="https://frutodelespiritu.com/images/logo.png"
-							alt="Bible study concept"
-							style={{
-								width: "50px",
-								height: "50px"
-							}}
-						/>
-						<div
-							style={{
-								display: "flex",
-								fontSize: 24,
-								fontStyle: "normal",
-								color: "gray",
-								marginTop: "auto",
-								whiteSpace: "pre-wrap"
-							}}>
-							<b>frutodelespiritu.com</b>
-						</div>
-					</div>
-					<div
-						style={{
-							display: "flex",
-							alignItems: "flex-start",
-							justifyContent: "flex-start",
-							flexDirection: "column",
-							fontStyle: "normal",
-							color: "black",
-							lineHeight: 1.8,
-							whiteSpace: "pre-wrap"
-						}}>
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "column",
-								alignItems: "flex-end"
-							}}>
-							<b
-								style={{
-									fontSize: 16,
-									whiteSpace: "pre-wrap",
-									color: "gray"
-								}}>
-								Reina Valera 1909
-							</b>
-							<b
-								style={{
-									fontSize: 50,
-									whiteSpace: "pre-wrap",
-									marginTop: "-20",
-									marginBottom: "20px"
-								}}>
-								Juan 3:16
-							</b>
-						</div>
-						<span
-							style={{
-								fontSize: 32,
-								whiteSpace: "pre-wrap",
-								textAlign: "start",
-								lineHeight: "1.5"
-							}}>
-							Porque de tal manera amó Dios al mundo para que todo aquel que
-							crea no se pierda, mas tenga vida eterna.
-						</span>
-					</div>
-				</div>
+        <ApiExportBaseImage
+          passageSelection={passageSelection}
+          mediaControllers={mediaControllers}
+          usage={usage}
+        />
+				// <div
+				// 	style={{
+				// 		height: "100%",
+				// 		width: "100%",
+				// 		display: "flex",
+				// 		textAlign: "center",
+				// 		alignItems: "flex-start",
+				// 		justifyContent: "space-between",
+				// 		flexDirection: "column",
+				// 		flexWrap: "nowrap",
+				// 		backgroundColor: "white",
+				// 		padding: 16,
+				// 		backgroundImage:
+				// 			"linear-gradient(to bottom left, #FED7AA, #FFEDD5, #FFF7ED)",
+				// 		backgroundRepeat: "no-repeat"
+				// 	}}>
+				// 	<div
+				// 		style={{
+				// 			position: "absolute",
+				// 			right: 16,
+				// 			bottom: 16,
+				// 			display: "flex",
+				// 			alignItems: "stretch",
+				// 			flexDirection: "row-reverse"
+				// 		}}>
+				// 		<img
+				// 			src="https://frutodelespiritu.com/images/logo.png"
+				// 			alt="Bible study concept"
+				// 			style={{
+				// 				width: "50px",
+				// 				height: "50px"
+				// 			}}
+				// 		/>
+				// 		<div
+				// 			style={{
+				// 				display: "flex",
+				// 				fontSize: 24,
+				// 				fontStyle: "normal",
+				// 				color: "gray",
+				// 				marginTop: "auto",
+				// 				whiteSpace: "pre-wrap"
+				// 			}}>
+				// 			<b>frutodelespiritu.com</b>
+				// 		</div>
+				// 	</div>
+				// 	<div
+				// 		style={{
+				// 			display: "flex",
+				// 			alignItems: "flex-start",
+				// 			justifyContent: "flex-start",
+				// 			flexDirection: "column",
+				// 			fontStyle: "normal",
+				// 			color: "black",
+				// 			lineHeight: 1.8,
+				// 			whiteSpace: "pre-wrap"
+				// 		}}>
+				// 		<div
+				// 			style={{
+				// 				display: "flex",
+				// 				flexDirection: "column",
+				// 				alignItems: "flex-end"
+				// 			}}>
+				// 			<b
+				// 				style={{
+				// 					fontSize: 16,
+				// 					whiteSpace: "pre-wrap",
+				// 					color: "gray"
+				// 				}}>
+				// 				Reina Valera 1909
+				// 			</b>
+				// 			<b
+				// 				style={{
+				// 					fontSize: 50,
+				// 					whiteSpace: "pre-wrap",
+				// 					marginTop: "-20",
+				// 					marginBottom: "20px"
+				// 				}}>
+				// 				Juan 3:16
+				// 			</b>
+				// 		</div>
+				// 		<span
+				// 			style={{
+				// 				fontSize: 32,
+				// 				whiteSpace: "pre-wrap",
+				// 				textAlign: "start",
+				// 				lineHeight: "1.5"
+				// 			}}>
+				// 			Porque de tal manera amó Dios al mundo para que todo aquel que
+				// 			crea no se pierda, mas tenga vida eterna.
+				// 		</span>
+				// 	</div>
+				// </div>
 			),
 
 			// Image options
 			{
 				width: 800,
-				height: 450, // Increased height so everything fits comfortably
+				height: 450,
 				quality: 100
 			}
 		);
---
+++ src/components/modals/ModalPromptUserToLogin.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import { serverBaseUrl } from "@/static";
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "flowbite-react";
+import { LordIconHover } from "@/components/animations/lordicon";
+import FINGERPRINT_SECURITY_LOTTIE from "@/lotties/fingerprint-security-hover-pinch.json";
+
+export default function ModalToPromptUserToLogin({
+	isModalToPromptUserToLoginOpen,
+	setIsModalToPromptUserToLoginOpen,
+	pathname
+}: {
+	isModalToPromptUserToLoginOpen: boolean;
+	setIsModalToPromptUserToLoginOpen: React.Dispatch<
+		React.SetStateAction<boolean>
+	>;
+	pathname: string;
+}) {
+	return (
+		<Modal
+			className="backdrop-blur-md bg-blue-50/10 dark:bg-gray-950/50"
+			dismissible
+			show={isModalToPromptUserToLoginOpen}
+			onClose={() => setIsModalToPromptUserToLoginOpen(false)}>
+			<ModalHeader className="bg-blue-100 dark:bg-gray-800 text-blue-950 dark:text-gray-50 border-b border-blue-200 dark:border-gray-600 p-5">
+				¿Cómo guardar versículos en listas de aprendizaje o tomar notas?
+			</ModalHeader>
+			<ModalBody>
+				<div className="space-y-6">
+					<p className="text-base leading-relaxed text-gray-900 dark:text-gray-200">
+						Cuando oprimes en un versículo, notarás que puedes hacer una
+						selección de los versículos que desees. Una vez seleccionados,
+						podrás añadirlos a tus listas de aprendizaje para memorizarlos o
+						para tomar notas sobre ellos.
+					</p>
+					<p className="text-base leading-relaxed text-gray-900 dark:text-gray-200">
+						Si eso es lo que quieres, debes iniciar sesión, todo lo que
+						necesitas es tu correo electrónico. Si no, puedes simplemente cerrar
+						esta ventana.
+					</p>
+				</div>
+			</ModalBody>
+			<ModalFooter className="border-t-blue-200 dark:border-gray-600 flex items-center justify-end">
+				<Link
+					className="p-2 text-sm font-medium text-center text-blue-50 rounded-2xl cursor-pointer bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-gray-900 dark:hover:bg-gray-800 dark:focus:ring-gray-800 transition-all duration-300 ease-in border border-blue-100 dark:border-gray-600 flex items-center gap-2"
+					href={`/log?${new URLSearchParams({
+						redirectUrl: serverBaseUrl + pathname
+					}).toString()}`}>
+					<LordIconHover
+						size={32}
+						ICON_SRC={FINGERPRINT_SECURITY_LOTTIE}
+						state="hover-pinch"
+						text="Iniciar sesión"
+					/>
+				</Link>
+			</ModalFooter>
+		</Modal>
+	);
+}
